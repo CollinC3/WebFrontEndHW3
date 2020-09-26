5eaec270bdb55549311f608bbcbffb942d44742e
CollinC3 <collinc1997@gmail.com>
2020-09-25 19:28:34 -0700

    Final Commit

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@
     'use strict';
     var FORM_SELECTOR = '[data-coffee-order="form"]';
     var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
+    
     var App = window.App;
     var Truck = App.Truck;
     var DataStore = App.DataStore;
@@ -10,12 +11,14 @@
 
     var myTruck = new Truck('ncc-1701', new DataStore());
     window.myTruck = myTruck;
-    var checklist = new CheckList(CHECKLIST_SELECTOR);
+
+    var checkList = new CheckList(CHECKLIST_SELECTOR);
+    checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
 
     var formHandler = new FormHandler(FORM_SELECTOR);
 
     formHandler.addSubmitHandler(function (data) {
         myTruck.createOrder.call(myTruck, data);
-        checklist.addRow.call(CheckList, data);
+        checkList.addRow.call(checkList, data);
     });
 })(window);
